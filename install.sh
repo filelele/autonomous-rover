4880e3f999c79371de519b99066c608572a5f743
filelele <lephi2712@outlook.com>
2026-08-21 15:32:33 +0700

    Chores: Change the way libdatachannel is installed to avoid duplicating download,  and more clear repo structure.

--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@
 # export TAILSCALE_PHONE_IP = "100.x.x.x"
 # export PI_IP="192.168.x.x"
 
+# Shared dependencies
+cd ./src/Shared
+if [ ! -d "libdatachannel" ]; then
+    git clone --recursive --depth 1 https://github.com/paullouisageneau/libdatachannel.git -b v0.23.3 libdatachannel
+fi
+cd ../..
+
 # Build for Phone side
 # create the apk for Phone
 cd ./src/Phone
@@ -12,7 +19,3 @@ bash ./create_apk.sh
 # create Server ground control station executable
 cd ../Server
 bash ./create_executable.sh
-
-
-
-
